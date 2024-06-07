b4a8f429692e444827d892a47635c4282301be6c
Perka Abhilasha <138596693+abhilashaperka@users.noreply.github.com>
2024-06-07 21:26:06 +0530

    Add files via upload

---
+++ anagram.py
@@ -0,0 +1,14 @@
+def valid_anagram(s,t):
+    if len(s)!=len(t):
+        return False
+    counter={}
+    for char in s:
+        counter[char]=counter.get(char,0)+1
+    for char in t:
+        if char not in counter or counter[char]==0:
+            return False
+        counter[char]-=1
+    return True
+s="anagram"
+t="nagaram"
+print(valid_anagram(s,t))
